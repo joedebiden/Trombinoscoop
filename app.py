8afc5641ddafd99db5831a360028312d2e707447
evan <Evan.Hugues@etu.univ-grenoble-alpes.fr>
2024-10-07 22:48:27 +0200

    login form check

--- app.py
+++ app.py
@@ -10,6 +10,23 @@ def root():
 def login():
     return render_template("login.html")
 
+def login(request):
+    if len(request.POST) > 0:
+        if 'email'not in request.POST or 'password' not in request.POST:
+            error = "Veuillez entrer une adresse de courriel et un mot de passe."
+            return render_template("login.html", {'error' : error})
+        else:
+            email = request.POST['email']
+            password = request.POST['password']
+
+            if password != 'sesame' or email != 'test@mail':
+                error = "Adresse mail ou mot de passe erroné."
+                return render_template('login.html', {'error' : error})
+            else:
+                return Flask.route('/welcome')
+    else:
+        return render_template('login.html')
+
 
 #permet de lancer le serveur juste en appelant le fichier python 
 if __name__ == '__main__':
