20cd5b29173a175c6ea3c2e45b08c9c6f3cd5f89
evan <Evan.Hugues@etu.univ-grenoble-alpes.fr>
2024-10-09 23:39:49 +0200

    views file added

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from models import db, Personne
+from models import db, Personne, Faculte, Campus
 from flask_admin import Admin
-
+from flask_admin.contrib.sqla import ModelView
+from views import *
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'clé_secrete'
@@ -13,48 +14,13 @@ db.init_app(app)
 
 admin = Admin(app, name='Admin Panel', template_mode='bootstrap4')
 
-
-#
-# create views.py file to import all the route
-#
-
-@app.route("/")
-@app.route("/welcome")
-def root():
-    return render_template("welcome.html")
-
-
-@app.route("/success")
-def success():
-    return redirect(url_for("root"))
-
-
-
-@app.route("/login", methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        if 'email' not in request.form or 'password' not in request.form:
-            error = "Veuillez entrer une addresse mail et un mot de passe"
-            return render_template('login.html', error=error)
-        else:
-            email = request.form['email']
-            password = request.form['password']
-            if email != 'test@mail':
-                error = "Adresse mail erroné."
-                return render_template('login.html', error=error)
-            elif password != 'test@mail':
-                error = "Mot de passe erroné."
-                return render_template('login.html', error=error)
-            else:
-                return redirect(url_for("success"))
-    else:
-        return render_template('login.html')
-
-
+admin.add_view(ModelView(Personne, db.session))
+admin.add_view(ModelView(Faculte, db.session))
+admin.add_view(ModelView(Campus, db.session))
 
 
 #permet de lancer le serveur juste en appelant le fichier python
 if __name__ == '__main__':
-    #with app.app_context():
-        #db.create_all()
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
---
+++ views.py
@@ -0,0 +1,33 @@
+from flask import render_template, request, redirect, url_for, flash
+from app import app
+
+@app.route("/")
+@app.route("/welcome")
+def root():
+    return render_template("welcome.html")
+
+
+@app.route("/success")
+def success():
+    return redirect(url_for("root"))
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        if 'email' not in request.form or 'password' not in request.form:
+            error = "Veuillez entrer une addresse mail et un mot de passe"
+            return render_template('login.html', error=error)
+        else:
+            email = request.form['email']
+            password = request.form['password']
+            if email != 'test@mail':
+                error = "Adresse mail erroné."
+                return render_template('login.html', error=error)
+            elif password != 'test@mail':
+                error = "Mot de passe erroné."
+                return render_template('login.html', error=error)
+            else:
+                return redirect(url_for("success"))
+    else:
+        return render_template('login.html')
